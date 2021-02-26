2749adf0a33a57b58593c8a46232d37644ba1af3
uria <uria1106@gmail.com>
2021-02-26 23:22:36 +0900

    簡単なカメラの追従を追加

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 offset = Vector3.zero;
+    public Vector3 NewPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Player == null)
+        {
+            Player = GameObject.Find("Player");
+            offset = this.transform.position - Player.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        NewPosition = this.transform.position;
+        NewPosition.x = Player.transform.position.x + offset.x;
+        //NewPosition.y = Player.transform.position.y + offset.y;
+        //NewPosition.z = Player.transform.position.z + offset.z;
+        this.transform.position = Vector3.Lerp(transform.position, NewPosition, 2.0f * Time.deltaTime);//時間をかけて元居た地点から次に行く地点に移動する
+    }
+}
