beddfe221ca6488879e29ff80edbf52c4d525b1a
uria <uria1106@gmail.com>
2021-03-08 23:31:41 +0900

    残像を表示する用のカメラを実装したけど微妙

---
+++ Assets/Script/EffectCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EffectCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RenderTexture[] TextureList;
+    public RawImage[] ImageList;
+    int count;
+    Camera MyCamera;
+    void Start()
+    {
+        MyCamera = GetComponent<Camera>();
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+        MyCamera.targetTexture = TextureList[count % TextureList.Length];
+        for (int i=0; i < ImageList.Length; i++){
+            int cal = (count % TextureList.Length + i)% ImageList.Length;
+            ImageList[i].texture = TextureList[cal];
+        }
+    }
+}
